36198c2498045e09e74ecb0d9bc53bace8749ebd
Yulia Sokolova <yuliasokolova@yahoo.com>
2019-03-13 19:39:01 -0400

    Assignment11-complete

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -1,21 +1,30 @@
 {
-  $('button#play-pause').on('click', function() {
-    player.playPause();
+  $('button#play-pause').on('click', function () {
+    helper.playPauseAndUpdate();
     $(this).attr('playState', player.playState);
   });
 
-  $('button#next').on('click', function(){
-    if(player.playState !== 'playing') {return;}
+  $('button#next').on('click', function () {
+    if (player.playState !== 'playing') {return;}
 
-    const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
-    const nextSongIndex = currentSongIndex + 1;
-    if (nextSongIndex >= album.songs.length) {return;}
-
+    const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);      const nextSongIndex = currentSongIndex + 1;
     const nextSong = album.songs[nextSongIndex];
-    player.playPause(nextSong);
+    helper.playPauseAndUpdate(nextSong);
+
+    if (nextSongIndex >= album.songs.length) {return;}
   });
 
-  $('#time-control input').on('input', function(event) {
+  $('button#previous').on('click', function () {
+    if (player.playState !== 'playing') {return;}
+
+    const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);      const previousSongIndex = currentSongIndex - 1;
+    const previousSong = album.songs[previousSongIndex];
+    helper.playPauseAndUpdate(previousSong);
+
+    if (previousSongIndex <= album.songs.length) {return;}
+  });
+
+  $('#time-control input').on('input', function (event) {
     player.skipTo(event.target.value);
   });
 
@@ -23,10 +32,12 @@
     if (player.playState !== 'playing') {return;}
       const currentTime = player.getTime();
       const duration = player.getDuration();
-      const percent = (currentTime/duration) * 100;
-      $('#time-control .current-time').text( currentTime );
+      const percent = (currentTime / duration) * 100;
+      $('#time-control .current-time').text(player.prettyTime(currentTime));
       $('#time-control input').val(percent);
   }, 1000);
 
-
+  $('#volume-control input').on('input', function (event) {
+    player.setVolume(event.target.value);
+  });
 }
